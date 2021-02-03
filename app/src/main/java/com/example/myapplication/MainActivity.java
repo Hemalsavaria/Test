b1162fc12d79cf040dfc9b8ed559805e354b006d
hemalsavaria@gmail.com <Hemal147056>
2021-02-03 18:36:38 +0530

    hemal

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -31,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Test", Toast.LENGTH_SHORT).show();
 
     }
 
