ad1684969d7fe41de7abc8310c9f98271f053454
Hemalsavaria <hemalsavaria@gmail.com>
2021-02-06 20:21:39 +0530

    pushed by Hemal on 4th feb 11:47 pm

--- app/src/main/java/com/example/myapplication/DashBoard.java
+++ app/src/main/java/com/example/myapplication/DashBoard.java
@@ -1,11 +1,11 @@
 package com.example.myapplication;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,7 +14,7 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 public class DashBoard extends AppCompatActivity {
 
-    LinearLayout dwr_doctor, dwr_chemist, dwr_atdnc, dwr_logout, dwr_mrs, dwr_sr,dwr_expense;
+    LinearLayout dwr_doctor, dwr_chemist, dwr_atdnc, dwr_logout, dwr_mrs, dwr_sr, dwr_expense, dwr_expense_daily,dwr_task,dwr_workplace,dwr_worktype;
     DrawerLayout drawer;
 
     @Override
@@ -46,7 +46,10 @@ public class DashBoard extends AppCompatActivity {
         dwr_atdnc = findViewById(R.id.dwr_atdnc);
         dwr_logout = findViewById(R.id.dwr_logout);
         dwr_expense = findViewById(R.id.dwr_expense);
-
+        dwr_expense_daily = findViewById(R.id.dwr_expense_daily);
+        dwr_task = findViewById(R.id.dwr_task);
+        dwr_workplace = findViewById(R.id.dwr_workplace);
+        dwr_worktype = findViewById(R.id.dwr_worktype);
 
 
         dwr_sr.setOnClickListener(new View.OnClickListener() {
@@ -103,9 +106,49 @@ public class DashBoard extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 drawer.close();
-                startActivity(new Intent(DashBoard.this, Attendence.class));
+                startActivity(new Intent(DashBoard.this, Expense.class));
             }
         });
+
+        dwr_expense_daily.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.close();
+                dilog_expense();
+            }
+        });
+        dwr_task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.close();
+                startActivity(new Intent(DashBoard.this, MR.class));
+            }
+        });
+
+        dwr_workplace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.close();
+                startActivity(new Intent(DashBoard.this, Workplace.class));
+            }
+        });
+
+        dwr_worktype.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.close();
+                startActivity(new Intent(DashBoard.this, Worktype.class));
+            }
+        });
+    }
+
+
+    public void dilog_expense() {
+        Dialog expense = new Dialog(DashBoard.this, R.style.MyAlertDialogStyle);
+        expense.setContentView(R.layout.dialog_daily_expense);
+
+        expense.show();
+
     }
 
     @Override
--- app/src/main/java/com/example/myapplication/Expense.java
+++ app/src/main/java/com/example/myapplication/Expense.java
@@ -1,16 +1,19 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 
-import android.view.View;
+import com.jaredrummler.materialspinner.MaterialSpinner;
 
 public class Expense extends AppCompatActivity {
+
+    CardView card_expense;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +22,20 @@ public class Expense extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        init();
+    }
+
+    public void init() {
+
+        card_expense = findViewById(R.id.card_expense);
+
+//
+
+        card_expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Expense.this, MR_expense.class));
+            }
+        });
     }
 }
